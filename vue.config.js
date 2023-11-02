9fa639c3600f65cb0178cd709b8c1cd4b031a40b
hieutst1997 <hieutst1997@gmail.com>
2023-11-02 16:10:05 +0700

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/hieutst1997/DemoGitIOVueJSApp' // repository
+      ? '/DemoGitIOVueJSApp' // repository
       : '/'
 }
